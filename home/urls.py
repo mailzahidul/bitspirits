59feafea2ce28669da4835b41f500e89cf4b6496
mailzahidul <mailzahidul@gmail.com>
2021-11-03 23:19:47 +0600

    third commit

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('courses/', views.courses, name='courses'),
+    path('lecturers/', views.lecturers, name='lecturers'),
+    path('news/', views.news, name='news'),
+    path('event/', views.event, name='event'),
     path('contact/', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,5 +7,31 @@ def home(request):
 
 
 
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def gallery(request):
+    return render(request, 'pages/gallery.html')
+
+
+def courses(request):
+    return render(request, 'pages/courses.html')
+
+
+def lecturers(request):
+    return render(request, 'pages/lecturers.html')
+
+
+def news(request):
+    return render(request, 'pages/news.html')
+
+
+def event(request):
+    return render(request, 'pages/event.html')
+
+
 def contact(request):
     return render(request, 'pages/contact.html')
+
+
